625293cde534b46ab7131adf771c1e6c3d695881
Yahor Shuliak <hangooover@gmail.com>
2023-09-22 19:43:57 +0300

    Initial

---
+++ BalinasoftTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BalinasoftTestApp
+//
+//  Created by Егор Шуляк on 22.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
